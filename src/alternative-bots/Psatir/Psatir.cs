e169f59e7793bc6786a350e907047e56e98e2a22
Muhammad Ra'if Alkautsar <96478279+lotsadelight@users.noreply.github.com>
2025-03-24 15:40:13 +0700

    fix: update to Psatir

--- src/alternative-bots/Psatir/Psatir.cs
+++ src/alternative-bots/Psatir/Psatir.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 // ------------------------------------------------------------------
 // Psatir
 // ------------------------------------------------------------------
-// The bot will ram you to death if it sees you weak
+// A spinning bot that greedily chases low-on-energy bots.
 // ------------------------------------------------------------------
 public class Psatir : Bot
 {
@@ -30,18 +30,19 @@ public class Psatir : Bot
         TurretColor = Color.Blue;
         RadarColor = Color.Black;
         ScanColor = Color.Yellow;
-        hittingWall = false;
+
+        hittingWall = false; // Boolean untuk mengetahui apakah bot baru saja menabrak tembok
         centerX = ArenaWidth / 2.0;
         centerY = ArenaHeight / 2.0;
         
-        while (IsRunning)
+        while (IsRunning) // Loop utama
         {
-            if (!hittingWall) {
+            if (!hittingWall) { // Bot berputar-putar dalam state normal
                 SetTurnLeft(1000);
                 SetForward(1000);
                 Go();
             }
-            else {
+            else { // Penanganan untuk state baru saja menabrak tembok
                 HandleHittingWall();
             }
         }
@@ -49,8 +50,10 @@ public class Psatir : Bot
 
     public override void OnScannedBot(ScannedBotEvent e)
     {
-        var distance = DistanceTo(e.X, e.Y);
+        var distance = DistanceTo(e.X, e.Y); // Hitung jarak ke bot yang di-scan
         double firepower;
+
+        // Hitung firepower berdasarkan jarak
         if (distance < 100) {
             firepower = 3;
         } else if (distance > 300) {
@@ -59,28 +62,28 @@ public class Psatir : Bot
             firepower = 3 - (((distance - 100)/200) * 2.5);
         }
 
-        if (e.Energy < 10)
+        // Jika musuh memiliki energi rendah, kejar dan ram!
+        if (e.Energy <= 24)
         {
             TurnToFaceTarget(e.X, e.Y);
             SetForward(distance + 5);
             SetRescan(); 
         }
 
-		if (Energy > 10) {
+        // Jika musuh masih memiliki energi tinggi, tembak.
+		if (Energy > 24) { 
         	SetFire(firepower);
 		}
         Go();
     }
 
-    private double NormalizeBearing(double angle) {
-        while (angle > 180) angle -= 360;
-        while (angle < -180) angle += 360;
-        return angle;
-    }
-
+    // Metode untuk penanganan jika menabrak bot.
     public override void OnHitBot(HitBotEvent e)
     {
+        // Hadapkan tank ke target.
         TurnToFaceTarget(e.X, e.Y);
+
+        // Memaksimalkan poin ramming dengan mengurangi firepower semakin rendah energi musuh.
         if (e.Energy > 16)
             SetFire(3);
         else if (e.Energy > 10)
@@ -96,6 +99,7 @@ public class Psatir : Bot
         Go();
     }
 
+    // Hadapkan tank ke target.
     private void TurnToFaceTarget(double x, double y)
     {
         var bearing = BearingTo(x, y);
@@ -104,14 +108,18 @@ public class Psatir : Bot
         Go();
     }
 
+
+    // Metode untuk penanganan kasus menabrak tembok
     private void HandleHittingWall() {
-        double distance = DistanceTo(centerX, centerY);
+        double distance = DistanceTo(centerX, centerY); // Hitung jarak ke pusat arena
         if (distance > 100) { 
+            // Jika jarak ke pusat masih lebih dari 100 pixel, tank bergerak ke pusat arena.
             double bearing = BearingTo(centerX, centerY);
             SetTurnLeft(bearing);
             SetForward(10_000);
         }
         else {
+            // Jika sudah dalam radius 100 dari pusat, kembali ke state default.
             hittingWall = false;
         }
         Go();
@@ -119,7 +127,7 @@ public class Psatir : Bot
 
     public override void OnHitWall(HitWallEvent e)
     {	
-        hittingWall = true;
+        hittingWall = true; // Aktifkan state hittingWall ketika menabrak tembok.
         Go();
     }
 }
